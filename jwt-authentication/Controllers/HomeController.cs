91b7fa101ab5d8cd24a44b89b9b8f7f80876974c
Sonu <sonusaha110@gmail.com>
2021-09-19 21:09:47 +0530

    Add a default api project, Install Microsoft.EntityFrameworkCore.Design Install Microsoft.EntityFrameworkCore.SqlServer Add DataContext class, user entity and do a migration to create the user table

---
+++ jwt-authentication/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jwt_authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "JWT", "Authentication" };
+        }
+    }
+}
---
+++ jwt-authentication/DataAccessLayer/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using jwt_authentication.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_authentication.DataAccessLayer
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDB;Initial Catalog=AuthDB;Integrated Security=true");
+            }
+        }
+    }
+}

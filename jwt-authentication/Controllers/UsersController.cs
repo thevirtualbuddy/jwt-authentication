473936de8b3fb14ca59ae5623489d6026b056faa
Sonu <sonusaha110@gmail.com>
2021-09-20 23:38:26 +0530

    Register api request method changed from query params to body

--- jwt-authentication/Controllers/UsersController.cs
+++ jwt-authentication/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿
 using jwt_authentication.DataAccessLayer;
+using jwt_authentication.DTO;
 using jwt_authentication.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,20 +31,29 @@ namespace jwt_authentication.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterUserDTO registerUser)
         {
+            //Check if user exists
+            //If we use ActionResult we are able to return different http status codes as response
+            if (await (UserExists(registerUser.Username))) return BadRequest("Username already exists");
+            
             // This is going to provide the hashing algorithm
             // With using we make sure that as soon as we're finished with the class, it's disposed correctly
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerUser.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerUser.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(u => u.UserName == username.ToLower());
         }
     }
 }

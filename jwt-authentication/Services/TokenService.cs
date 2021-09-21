636fa6759c3ba01d505d1750c56383418e69fcfc
Sonu <sonusaha110@gmail.com>
2021-09-21 23:57:27 +0530

    Add ITokenService and Token Service Dependency injection in startup.cs of ITokenService

---
+++ jwt-authentication/Services/TokenService.cs
@@ -0,0 +1,17 @@
+﻿using jwt_authentication.Entities;
+using jwt_authentication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_authentication.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

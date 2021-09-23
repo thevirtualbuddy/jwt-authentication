e1d69c4704efba1a372a3eda5120edd7f3211565
Sonu <sonusaha110@gmail.com>
2021-09-24 00:12:34 +0530

    Add service for authentication in ConfigureServices method of startup.cs Specify the authentication middleware in Configure method of startup.cs

--- jwt-authentication/Startup.cs
+++ jwt-authentication/Startup.cs
@@ -1,6 +1,7 @@
 using jwt_authentication.DataAccessLayer;
 using jwt_authentication.Interfaces;
 using jwt_authentication.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,19 +11,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace jwt_authentication
 {
     public class Startup
     {
+        
         private readonly IConfiguration _config;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -35,6 +39,19 @@ namespace jwt_authentication
 
             services.AddDbContext<DataContext>();
             services.AddControllers();
+
+            // Add service for our authentication 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+                        ValidateIssuer = false,     // Issuer of the token is our API server
+                        ValidateAudience = false,   // Audience of the token would be the front-end application
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +66,8 @@ namespace jwt_authentication
 
             app.UseRouting();
 
+            // Mention the authentication middle ware: Ordering is important
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

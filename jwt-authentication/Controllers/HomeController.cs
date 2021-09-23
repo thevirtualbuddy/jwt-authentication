c8757dfdc590b9fe2bcac009600cb60a36046ebf
Sonu <sonusaha110@gmail.com>
2021-09-23 23:51:16 +0530

    Add anonymous access to fetch all users: for testing

--- jwt-authentication/Controllers/HomeController.cs
+++ jwt-authentication/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using jwt_authentication.DataAccessLayer;
+using jwt_authentication.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +12,39 @@ using System.Threading.Tasks;
 
 namespace jwt_authentication.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/<HomeController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<string>> GetDummy()
         {
-            return new string[] { "JWT", "Authentication" };
+            return "JWT Authentication";
         }
+
+        // Testing to fetch user anonymously
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+
     }
 }
